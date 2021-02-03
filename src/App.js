11a12a124611916690ec81e748b99a96cc3b21f7
ppambler <allinlbj@gmail.com>
2021-02-03 19:00:46 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        你点击了 <span id="value">{this.props.n}</span> 次
+        <div>
+          <button id="add1" onClick={() => this.props.add1()}>
+            +1
+          </button>
+          <button id="add2" onClick={() => this.props.add2()}>
+            +2
+          </button>
+          <button
+            id="add1IfOdd"
+            onClick={() => this.props.add1IfOdd(this.props.n)}
+          >
+            如果是单数就+1
+          </button>
+          <button id="add1After2Sec" onClick={() => this.props.add1After2Sec()}>
+            两秒后+1
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    n: state.n,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    add1: () => dispatch({ type: "add", payload: 1 }),
+    add2: () => dispatch({ type: "add", payload: 2 }),
+    add1IfOdd: function (n) {
+      if (n % 2 === 1) {
+        return dispatch({ type: "add", payload: 1 });
+      }
+    },
+    add1After2Sec: function () {
+      setTimeout(() => {
+        return dispatch({ type: "add", payload: 1 });
+      }, 2000);
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);

801931914207f1a9c12cce8c436c8f4c874aef6a
ppambler <allinlbj@gmail.com>
2021-02-04 00:34:03 +0800

    demo5 homework

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react'
-console.log(React)
-console.log(Component)
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-class App extends Component {
-  add1() {
-    this.props.store.dispatch({type:'add',payload:1})
-  }
-  add2() {
-    this.props.onAdd2()
-  }
-  add3() {
-    this.props.onAdd3()
-  }
-  add4() {
-    this.props.onAdd4()
-  }
-  render() {
-    return (
-      <div>
-        你点击了 <span id="value">{this.props.value}</span> 次
-        <div>
-          <button id="add1" onClick={() => this.add1()}>+1</button>
-          <button id="add2" onClick={() => this.add2()}>+2</button>
-          <button id="add1IfOdd" onClick={() => this.add3()}>如果是单数就+1</button>
-          <button id="add1After2Sec" onClick={() => this.add4()}>两秒后+1</button>
-        </div>
-      </div>
-    )
-  }
-
+function App(props) {
+  return (
+    <div>
+      {props.n}
+      <button
+        onClick={() => {
+          props.add1();
+        }}
+      >
+        +1
+      </button>
+    </div>
+  );
 }
 
-export default App;
+function x(state) {
+  return {
+    n: state.n,
+  };
+}
+
+function y(dispatch) {
+  return {
+    add1: () => dispatch({ type: "add", payload: 1 }),
+  };
+}
+
+export default connect(x, y)(App);

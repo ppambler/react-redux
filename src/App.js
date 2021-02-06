41980feed17946da5c6cb07642239529b98c497e
ppambler <allinlbj@gmail.com>
2021-02-06 10:35:30 +0800

    更新 Context 里边的值

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React, { Component } from 'react'
-console.log(React)
-console.log(Component)
-
+import React,{Component} from "react"
+import ReactDOM from "react-dom";
+import ProductList from "./Components/ProductList";
 class App extends Component {
-  add1() {
-    this.props.store.dispatch({type:'add',payload:1})
+  state = {
+    cars: {
+      car001: { name: "Honda", price: 100 },
+      car002: { name: "BMW", price: 150 },
+      car003: { name: "Mercedes", price: 200 },
+    },
+  };
+  incrementCarPrice = this.incrementCarPrice.bind(this);
+  decrementCarPrice = this.decrementCarPrice.bind(this);
+
+  incrementCarPrice(selectedID) {
+    // a simple method that manipulates the state
+    const cars = Object.assign({}, this.state.cars);
+    cars[selectedID].price = cars[selectedID].price + 1;
+    this.setState({
+      cars,
+    });
   }
-  add2() {
-    this.props.onAdd2()
+
+  decrementCarPrice(selectedID) {
+    // a simple method that manipulates the state
+    const cars = Object.assign({}, this.state.cars);
+    cars[selectedID].price = cars[selectedID].price - 1;
+    this.setState({
+      cars,
+    });
   }
-  add3() {
-    this.props.onAdd3()
-  }
-  add4() {
-    this.props.onAdd4()
-  }
+
   render() {
     return (
-      <div>
-        你点击了 <span id="value">{this.props.value}</span> 次
-        <div>
-          <button id="add1" onClick={() => this.add1()}>+1</button>
-          <button id="add2" onClick={() => this.add2()}>+2</button>
-          <button id="add1IfOdd" onClick={() => this.add3()}>如果是单数就+1</button>
-          <button id="add1After2Sec" onClick={() => this.add4()}>两秒后+1</button>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to my web store</h1>
+        </header>
+        {/* Pass props twice */}
+        <ProductList
+          cars={this.state.cars}
+          incrementCarPrice={this.incrementCarPrice}
+          decrementCarPrice={this.decrementCarPrice}
+        />
       </div>
-    )
+    );
   }
-
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,52 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
+function F1(props) {
+  return (
+    <div className="bordered">
+      1111
+      <F2 />
+    </div>
+  );
+}
+function F2(props) {
+  return (
+    <div className="bordered">
+      2222
+      <F3 />
+    </div>
+  );
+}
+function F3(props) {
+  return (
+    <div className="bordered">
+      3333
+      <nContext.Consumer>
+        {(x) => <F4 n4={x.n} setN={x.setN} />}
+      </nContext.Consumer>
+    </div>
+  );
+}
+function F4(props) {
+  return (
+    <div className="bordered">
+      4444, {props.n4}
+      <button onClick={props.setN}>Click me</button>
+    </div>
+  );
+}
 
-const stateChanger = (state, action) => {
-  if (state === undefined) {
-    return 0
-  } else {
-    if (action.type === 'add') {
-      var newState = state + action.payload
-      return newState
-    } else {
-      return state
-    }
+const nContext = React.createContext();
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      x: {
+        n: 67,
+        setN: () => {
+          this.setState({
+            x: {
+              ...this.state.x,
+              n: this.state.x.n + 1,
+            },
+          });
+          console.log("执行完毕");
+        },
+      },
+    };
   }
-
-  return newState
-}
-const store = createStore(stateChanger)
-render()
-store.subscribe(() => {
-  render()
-})
-
-function add3() {
-  if (store.getState() % 2 === 1) {
-    store.dispatch({ type: 'add', payload: 1 })
+  render() {
+    return (
+      <div>
+        <nContext.Provider value={this.state.x}>
+          <F1 />
+        </nContext.Provider>
+      </div>
+    );
   }
 }
-function add4() {
-  setTimeout(() => {
-    store.dispatch({ type: 'add', payload: 1 })
-  }, 2000)
-}
 
-function render() {
-  ReactDOM.render(<App value={store.getState()} store={store}
-    // onAdd1={() => { store.dispatch({ type: 'add', payload: 1 }) }}
-    onAdd2={() => { store.dispatch({ type: 'add', payload: 2 }) }}
-    onAdd3={add3}
-    onAdd4={add4}
-  />, document.getElementById('root'));
-}
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);

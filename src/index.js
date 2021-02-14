8724d064681418fe881008c0396c123be7fb6691
ppambler <allinlbj@gmail.com>
2021-02-14 13:11:09 +0800

    demo9

--- src/index.js
+++ src/index.js
@@ -1,50 +1,107 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
+let div = document.createElement("div");
+document.body.appendChild(div);
 
-const stateChanger = (state, action) => {
-  if (state === undefined) {
-    return 0
-  } else {
-    if (action.type === 'add') {
-      var newState = state + action.payload
-      return newState
-    } else {
-      return state
-    }
+console.log = function (content) {
+  div.innerHTML += `${content}<br>`;
+};
+
+class Baba extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hasChild: true,
+    };
   }
-
-  return newState
-}
-const store = createStore(stateChanger)
-render()
-store.subscribe(() => {
-  render()
-})
-
-function add3() {
-  if (store.getState() % 2 === 1) {
-    store.dispatch({ type: 'add', payload: 1 })
+  onClick() {
+    this.setState({
+      hasChild: false,
+    });
+  }
+  callSon() {
+    this.setState({
+      word: "你还好吧",
+    });
+  }
+  createSon() {
+    this.setState({
+      hasChild: true
+    })
+  }
+  render() {
+    return (
+      <div className="baba">
+        我是你爸爸
+        <button onClick={() => this.onClick()}>kill son</button>
+        <button onClick={() => this.callSon()}>call son</button>
+        <button onClick={() => this.createSon()}>create son</button>
+        {this.state.hasChild ? <App word={this.state.word} /> : null}
+      </div>
+    );
   }
 }
-function add4() {
-  setTimeout(() => {
-    store.dispatch({ type: 'add', payload: 1 })
-  }, 2000)
+
+class App extends React.Component {
+  add1() {
+    console.log("用户点击了");
+    this.setState({
+      n: this.state.n + 1,
+      flag: true,
+    });
+  }
+  constructor() {
+    super();
+    this.state = {
+      n: 0,
+      flag: false,
+    };
+    // console.log(this.state)
+    console.log("创建 App");
+  }
+  componentWillMount() {
+    // console.log(this.state)
+    console.log("将要 mount App");
+  }
+  render() {
+    console.log(`${this.state.flag ? "更新" : "填充"} App 的内容`);
+    return (
+      <div className="app">
+        {this.state.n}
+        <br />
+        我爸说 {this.props.word}
+        <br />
+        <button
+          onClick={() => {
+            this.add1();
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+  componentDidMount() {
+    console.log("mount App 完毕");
+  }
+  componentWillUpdate() {
+    console.log("update App 之前");
+  }
+  componentDidUpdate() {
+    console.log("update App 之后");
+  }
+   componentWillUnmount() {
+    console.log("App 快要死了，记得喂狗");
+  }
+  componentWillReceiveProps() {
+    console.log("我爸说话了");
+  }
 }
 
-function render() {
-  ReactDOM.render(<App value={store.getState()} store={store}
-    // onAdd1={() => { store.dispatch({ type: 'add', payload: 1 }) }}
-    onAdd2={() => { store.dispatch({ type: 'add', payload: 2 }) }}
-    onAdd3={add3}
-    onAdd4={add4}
-  />, document.getElementById('root'));
-}
+ReactDOM.render(<Baba />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

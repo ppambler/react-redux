18d2aada0e689441e927b0e3f600d849b108ab6e
ppambler <allinlbj@gmail.com>
2021-02-17 00:50:41 +0800

    涟漪组件

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React, { Component } from 'react'
-console.log(React)
-console.log(Component)
+import React, { Component } from 'react';
+import './App.css';
+import Button from './components/Button'
 
 class App extends Component {
-  add1() {
-    this.props.store.dispatch({type:'add',payload:1})
-  }
-  add2() {
-    this.props.onAdd2()
-  }
-  add3() {
-    this.props.onAdd3()
-  }
-  add4() {
-    this.props.onAdd4()
-  }
   render() {
     return (
-      <div>
-        你点击了 <span id="value">{this.props.value}</span> 次
-        <div>
-          <button id="add1" onClick={() => this.add1()}>+1</button>
-          <button id="add2" onClick={() => this.add2()}>+2</button>
-          <button id="add1IfOdd" onClick={() => this.add3()}>如果是单数就+1</button>
-          <button id="add1After2Sec" onClick={() => this.add4()}>两秒后+1</button>
-        </div>
+      <div className="App">
+        <Button value="按钮" onClick={this.z.bind(this)} />
+        <Button value="按钮" />
+        <Button value="按钮" />
       </div>
-    )
+    );
   }
-
+  z(klsjadlksajdlksajdlksjlsjl) {
+    console.log('外面的函数')
+    console.log(klsjadlksajdlksajdlksjlsjl);
+  }
 }
 
 export default App;
